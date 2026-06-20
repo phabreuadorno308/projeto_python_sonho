7480cd0ac9deda724c3527eab143e69d8afe4b02
phabreuadorno308 <phabreuadorno@usp.br>
2026-06-20 20:43:37 -0300

    Adicionando lógica inicial da nossa função, em conjunto das dúvidas que tivemos

--- function.py
+++ function.py
@@ -1,10 +1,44 @@
-def calculate_velocity(target, current):
+def calculate_velocity(target: tuple, current: tuple) -> list:
+    """
+    Função que calcula velocidade em plano bidimensional
+
+    Args:
+        target (tupla): posição alvo do robô ([px, py, yaw])
+        current (tupla): posição atual do robô ([rx, ry, ryaw])
+    Returns:
+        vel (list): lista com comando de velocidade ([vx, w])
+    """
+    
+    # Constantes
+    alfa = 0.5 # -> quão angular vai
+    beta = 0.5 # -> quão linear vai
+
+    # vel = [vx, w] -> Padrão nula
     vel = [0.0, 0.0]
 
-    # Sua lógica de comando
+    # Primeiro ajusta ângulo
+    dif_ang = target[2] - current[2]
+
+    if dif_ang > 0:
+        avan_ang = alfa * dif_ang
+        vel = [0, avan_ang]
+    elif dif_ang < 0:
+        avan_ang = -(alfa * dif_ang)
+        vel = [0.0, avan_ang]
+    
+    # Ajusta avanço linear
+    else:
+        delta_x = target[0] - current[0]
+        delta_y = target[1] - current[1]
+        dif_lin = (delta_x**2 + delta_y**2)**(1/2)
+
+        if dif_lin != 0:
+            avan_lin = beta * dif_lin
+            vel = [avan_lin, 0.0]
 
     return vel
 
-# goal = [px, py, yaw]
-# current = [rx, ry, ryaw]
-# vel = [vx, w]
+
+# --- Dúvidas ---
+# 1. Código está do jeito que esperam (quão complexo deve ser)
+# 2. Se considera vel atual lin e ang (visto que não há como ter histórico sem var estáticas e sem main)
