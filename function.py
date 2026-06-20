e84e00eae85dbe9eb118cb01c8a4d7f3c58675f0
phabreuadorno308 <phabreuadorno@usp.br>
2026-06-20 19:52:44 -0300

    Criação arquivo função

---
+++ function.py
@@ -0,0 +1,10 @@
+def calculate_velocity(target, current):
+    vel = [0.0, 0.0]
+
+    # Sua lógica de comando
+
+    return vel
+
+# goal = [px, py, yaw]
+# current = [rx, ry, ryaw]
+# vel = [vx, w]
